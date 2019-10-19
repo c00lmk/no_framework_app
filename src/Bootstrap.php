cccca8c58d0c71fec28c07e776118c1ee88d72ba
c00lmk <maciek.kasprzak@coolmaciek.pl>
2019-10-19 21:42:19 +0200

    Adds new Error Handler: Whoops

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,3 +1,26 @@
 <?php declare(strict_types = 1);
 
+namespace no_framework_app;
+
+require  __DIR__ . '/../vendor/autoload.php';
+
+error_reporting(E_ALL);
+
+$environment = 'development';
+
+/**
+ * Register the error handler
+ */
+$whoops = new \Whoops\Run;
+if ($environment !== 'production') {
+    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+} else {
+    $whoops->pushHandler(function($e) {
+        echo "Todo: Friendly error page and send email to developer";
+    });
+}
+$whoops->register();
+
+throw new \Exception;
+
 echo 'Hello World!';
